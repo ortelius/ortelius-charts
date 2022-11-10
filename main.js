6cc1adf8c9bc125492afe4140800f6c0bef20ac8
Steve Taylor <steve@deployhub.com>
2022-11-10 11:18:15 -0700

    gen main chart and then helm index

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const axios = require("axios");
 const yaml = require('js-yaml');
 const fs = require('fs');
+const { exit } = require("process");
 
 const chartRepos = [
     "ortelius/ortelius",
-    "ortelius/ortelius-ms-dep-pkg-cud",
-    "ortelius/ortelius-ms-dep-pkg-r",
-    "ortelius/ortelius-ms-textfile-crud",
-    "ortelius/ortelius-ms-compitem-crud",
-    "ortelius/ortelius-ms-validate-user",
+    "ortelius/ms-dep-pkg-cud",
+    "ortelius/ms-dep-pkg-r",
+    "ortelius/ms-textfile-crud",
+    "ortelius/ms-compitem-crud",
+    "ortelius/ms-validate-user",
+    "ortelius/ms-scorecard",
 ]
 
 // Helper functions
 async function getChartEntries() {
+    await axios.get('https://raw.githubusercontent.com/ortelius/ortelius/main/chart/Chart.yaml').then(response => {
+        let parsedYaml = yaml.load(response.data)
+        chartVersion = parsedYaml['version'];
+        parts = chartVersion.split('.');
+        ver = parseInt(parts[2]) + 1;
+        parts[2] = ver.toString();
+        chartVersion = parts.join('.');
+    });
+
+    latest_chart = [];
+
     for (let i = 0; i < chartRepos.length; i++) {
         const repoUrl = `https://github.com/${chartRepos[i]}/raw/gh-pages/index.yaml`;
         
@@ -21,28 +34,57 @@ async function getChartEntries() {
             let entries = parsedYaml.entries
 
             Object.keys(entries).forEach(key => {
-                chartEntries[key] = entries[key]
+
+                latest = null;
+
+                Object.entries(entries[key]).forEach(entry => {
+                    
+                    if (latest == null)
+                        latest = entry;
+                    else if (latest['created'] < entry['created'])
+                        latest = entry;
+                });
+                latest = latest[1];
+                dep = {};
+                dep['name'] = latest['name'];
+                dep['version'] = latest['version'];
+                dep['repository'] = 'https://ortelius.github.io/' + key + '/'
+                latest_chart.push(dep);
+
+            //    chartEntries[key] = entries[key]
+            //    console.log(entries[key]);
             });
-            return entries
         })
     }
+    chartEntries = latest_chart;
+    return latest_chart;
 }
+
 function createYamlOutput() {
     const output = yaml.dump({
-        apiVersion: "v1",
-        entries: chartEntries,
-        generated: new Date().toISOString()
+        apiVersion: 'v2',
+        name: 'ortelius',
+        cdescription: 'Ortelius',
+        home: 'https://www.ortelius.io',
+        icon: 'https://ortelius.github.io/ortelius-charts/logo.png',
+        keywords: [ 'Service Catalog', 'Microservices', 'SBOM' ],
+        type: 'application',
+        version: chartVersion,
+        appVersion: '10.0.0',
+        generated: new Date().toISOString(),
+        dependencies: chartEntries,
     }, {noArrayIndent: true})
 
     return output
 }
 // -----------------
 
-let chartEntries = {}
+let chartEntries = []
+let chartVersion = ''
 
 getChartEntries().then(() => {
     const yamlOutput = createYamlOutput()
-
+    console.log(yamlOutput);
     fs.writeFileSync("./index-temp.yaml", yamlOutput, "utf8", (err) => {
         console.log(err)
     })
